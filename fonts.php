f8e41f807fa083d963b21606a07e0a84ff08c590
Dewanik <kdewanik@gmail.com>
2018-11-04 10:17:24 +0545

    Some Landpage and CSS Changes

---
+++ fonts.php
@@ -0,0 +1,6 @@
+<!--Add all fonts link here -->
+<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Parisienne" rel="stylesheet">
--- index.php
+++ index.php
@@ -1,10 +1,20 @@
+<?php include 'fonts.php';
+
+ ?>
 <!DOCTYPE html>
+
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>School Interaction System</title>
+    <link rel='shortcut icon' href='https://lh3.googleusercontent.com/7TEMGSYMfOCJdVYM9iNye4w0YP28ODGo3y6soNQB0sGRfFrtmP1EClvIS3TBU6qWYng=s180'/>
+<link rel="stylesheet" href="css/mainpage.css">
   </head>
   <body>
-
+    <img class='blmiki-logo' src='https://lh3.googleusercontent.com/7TEMGSYMfOCJdVYM9iNye4w0YP28ODGo3y6soNQB0sGRfFrtmP1EClvIS3TBU6qWYng=s180'/>
+<h1 class='typewriter'>Welcome To School Interaction / Managment System  </h1>
   </body>
+  <footer>
+  <h6>Developed with &hearts; By Dewanik , Ashim</h6>
+</footer>
 </html>
