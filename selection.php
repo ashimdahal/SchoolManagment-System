81241b49d3bb41e827f23faa6f1604796c4a34f9
ashim dahall <bboxchmpx@gmail.com>
2018-11-08 17:33:57 +0545

    fb ma bhaneko changes heru

---
+++ selection.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/mainpage.css">
+    <link rel='shortcut icon' href='https://lh3.googleusercontent.com/7TEMGSYMfOCJdVYM9iNye4w0YP28ODGo3y6soNQB0sGRfFrtmP1EClvIS3TBU6qWYng=s180'/>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+
+    <title>Selection</title>
+  </head>
+  <body>
+
+      <h1>select the user<h1>
+
+        <!-- flexbox-->
+
+<div class="gallery-container">
+  <!-- to hold for teacher and for students respectively-->
+
+  <a href="teachlogin.php">
+  <div class="teacher">
+<i class="fas fa-chalkboard-teacher fa-4x"></i><br>Teacher
+  </div>
+  </a>
+  <a href="stdlogin.php">
+  <div class="student">
+<i class="fas fa-user-graduate fa-4x"></i><br>Student
+  </div>
+  </a>
+</div>
+
+
+  </body>
+</html>
